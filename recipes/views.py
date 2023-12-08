0bc18d8036ef3df734c45725c5183328932c1580
HeithorUrquiza <heithoru@gmail.com>
2023-12-08 14:50:41 -0300

    Studing enhance

--- recipes/views.py
+++ recipes/views.py
@@ -6,4 +6,4 @@ def home(req):
     return render(req, 'recipes/pages/home.html')
 
 def recipes(req, id):
-    return render(req, 'recipes/pages/home.html')
+    return render(req, 'recipes/pages/recipe-view.html')
