5fd146c6b101cbf98bcf8fcdcd8b336cb3c1bfaa
HeithorUrquiza <heithoru@gmail.com>
2023-12-11 10:13:33 -0300

    Learning Django ORM

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+# recipes:home e.g
+app_name = 'recipes'
+
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipes),
+    path('', views.home, name='home'),
+    path('recipes/<int:id>/', views.recipes, name='recipe'),
 ]
