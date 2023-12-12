fea4e2e73532e278004a0fd965413f5a476db97b
HeithorUrquiza <heithoru@gmail.com>
2023-12-12 09:09:08 -0300

    Dealing with category null values

--- recipes/views.py
+++ recipes/views.py
@@ -5,8 +5,17 @@ from .models import Recipe
 
 # Create your views here.
 def home(req):
-    recipes = Recipe.objects.all()
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(req, 'recipes/pages/home.html', context={
+        'recipes': recipes,
+    })
+
+def category(req,category_id):
+    recipes = Recipe.objects.filter(
+        category__id=category_id,
+        is_published=True,
+        ).order_by('-id')
+    return render(req, 'recipes/pages/category.html', context={
         'recipes': recipes,
     })
 
